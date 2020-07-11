8e8a516119580417fbe225545c94dff9a4832e6b
Rodion Artyukhin <artyukhin.rodion@gmail.com>
2020-07-11 15:00:59 +0300

    Initial Commit

---
+++ AdaptiveLayoutUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveLayoutUIKit
+//
+//  Created by Родион on 11.07.2020.
+//  Copyright © 2020 Trinity Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
